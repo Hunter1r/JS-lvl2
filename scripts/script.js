05e285ba7d93635ca100d9f5216a052ae1272455
Hunter1r <Hunter1r@yandex.ru>
2020-04-20 18:36:36 +0500

    Убрал параметр по умолчанию из renderGoodsList

--- scripts/script.js
+++ scripts/script.js
@@ -10,8 +10,8 @@ const goods =
 const renderGoodsItem = (title, price) => { 
     return `<div class="goods-item"><h3>​ ${title}​</h3><p> ${price}​</p></div>`; 
 };
-const renderGoodsList = (list=goods) => {
+const renderGoodsList = (list) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price)); 
     document.querySelector('.goods-list').innerHTML = goodsList.join(''); 
 }
-renderGoodsList(); //массив goods указывается в функции по умолчанию (параметра list)
+renderGoodsList(goods);
