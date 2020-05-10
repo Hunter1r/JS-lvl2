3adeb0f96b4580995e70f6bb7e97d473a8c5c49f
Hunter1r <Hunter1r@yandex.ru>
2020-05-10 23:08:44 +0500

    В тексте 't не подменяет на двойную кавычку, а оставляет одинарную

--- scripts/schablon.js
+++ scripts/schablon.js
@@ -2,12 +2,9 @@
 
 let text = "Lorem 'ipsum' sit aren't ASD 'qwerty' ";
 
- const regexp = /'/g;
- //let newstr = text.match(regexp);
- let newstr = text.replace(regexp, '"');
+const regexp = /'/g;
+let newstr = text.replace(regexp, '"');
+let result = newstr.replace(/"t/g, "'t")
 
- //let newnew = newstr.replace(/"t/g,"'t")
-  
- console.log(text);
- console.log(newstr);
-  
+console.log(text);
+console.log(result);
