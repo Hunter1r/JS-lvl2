0050e78be69c1d0d70bdd96c04fe45319d62fb40
Hunter1r <Hunter1r@yandex.ru>
2020-05-26 00:14:26 +0500

    Удалена строка вывода сообщения в консоль

--- scripts/script.js
+++ scripts/script.js
@@ -240,7 +240,6 @@ const app = new Vue({
                     callback(xhr.responseText);
                 }
             }
-            console.log('data='+data);
             xhr.open('POST', url, true);
             xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
             xhr.send(JSON.stringify(data));
