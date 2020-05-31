c7de0ca575b3ea4be17d69465fa8da6730e4bb21
Hunter1r <Hunter1r@yandex.ru>
2020-05-31 13:20:53 +0500

    Подключаемые модули для Webpack

---
+++ script.js
@@ -0,0 +1,9 @@
+import module from './module_vue_main.js';
+import module_vue_search from './module_vue_search.js';
+import module_vue_basket from './module_vue_basket.js';
+
+const app = module.app
+const Vue = module_vue_search.vue
+const Vue2 = module_vue_basket.vue
+
+
